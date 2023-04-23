052deea5ad70b0a79b9d76767d953812e530a9e6
radyaadi <adiradya140@gmail.com>
2023-04-23 09:24:42 +0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<script setup>
+// import { RouterLink, RouterView } from 'vue-router';
+</script>
+
+<template>
+  <header>
+    <div class="header__brand container">
+      <h1>Tv Maze</h1>
+    </div>
+  </header>
+  <main class="container">
+    <RouterView />
+  </main>
+  <footer>
+    <p>
+      Created by
+      <a
+        href="https://radya-profile-web.netlify.app/"
+        target="_blank"
+        rel="noreferrer"
+        >Radya Adi</a
+      >
+    </p>
+  </footer>
+</template>
---
+++ src/views/MoviesList.vue
@@ -0,0 +1,44 @@
+<script>
+import axios from 'axios';
+export default {
+  data() {
+    return { movies: [] };
+  },
+  mounted() {
+    axios.get('http://api.tvmaze.com/search/shows?q=girls').then((res) => {
+      this.movies = res.data;
+    });
+  },
+};
+</script>
+
+<template>
+  <div class="movies">
+    <div class="movie__item" v-for="movie in movies" :key="movie.show.id">
+      <div class="movie-item__header">
+        <img
+          class="movie-item__header-poster"
+          :alt="movie.show.name"
+          :src="movie.show.image.medium"
+        />
+        <div class="movie-item__header-rating">
+          <p>
+            ⭐️<span class="movie-item__header-rating-score">
+              {{
+                movie.show.rating.average
+                  ? movie.show.rating.average
+                  : 'Unrated'
+              }}
+            </span>
+          </p>
+        </div>
+      </div>
+      <div class="movie-item__content">
+        <h3>
+          {{ movie.show.name }}
+        </h3>
+        <p v-html="movie.show.summary"></p>
+      </div>
+    </div>
+  </div>
+</template>
